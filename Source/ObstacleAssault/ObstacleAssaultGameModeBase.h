ff3b10a78bd9e8ec52da9d116820c0603dc83492
Reza Hakhamanesh <63959403+EschatonMind@users.noreply.github.com>
2023-08-11 15:03:24 +0330

    Initialize
    Initialize

---
+++ Source/ObstacleAssault/ObstacleAssaultGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ObstacleAssaultGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OBSTACLEASSAULT_API AObstacleAssaultGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
